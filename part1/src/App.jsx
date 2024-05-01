ca6048018d4e95fb5516256cb541aa814f7b92e6
benny <bennyrobot1143@gmail.com>
2024-05-01 22:49:23 +0800

    complete part 1.1-1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,51 @@
-const App = () => {
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
+  const contents = props.parts.map(({ name, exercises }) => <Part name={name} exercises={exercises} />)
   return (
     <div>
-      <p>Hello world</p>
+      {contents}
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises}</p>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    },
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises={parts.reduce((accumulator, { exercises }) => accumulator + exercises, 0)} />
     </div>
   )
 }
